1fea07364c90c97eb016529e868b2d93537d0ae2
JeetKumbhar <jeetkumbhar2210@gmail.com>
2026-08-23 17:41:14 +0530

    GSAP SVG Animation

---
+++ 5. SVG Animations/script.js
@@ -0,0 +1,23 @@
+var path = `M 10 200 Q 650 200 1300 200`
+
+var finalPath = `M 10 200 Q 650 200 1300 200`
+
+var string = document.querySelector("#string")
+
+string.addEventListener("mousemove", function(dets){
+    path = `M 10 200 Q ${dets.x} ${dets.y} 1300 200`
+
+    gsap.to("svg path", {
+        attr: {d:path},
+        duration:0.3,
+        ease: "power3.out"
+    })
+})
+
+string.addEventListener("mouseleave", function(){
+    gsap.to("svg path", {
+        attr: {d:finalPath},
+        duration: 1.5,
+        ease: "elastic.out(1, 0.2)"
+    })
+})
